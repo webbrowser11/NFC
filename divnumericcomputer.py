3fc04dc012f3883eb015565db2e19979a5542cc0
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-17 16:22:44 -0800

    add the odd/even number

---
+++ divnumericcomputer.py
@@ -0,0 +1,13 @@
+# division numeric computer
+Input = int(input("Enter a number: "))
+
+def inputcell(layer, input):
+    global Dump1, Dump2
+    Dump1 = input / layer
+    Dump2 = input / layer
+
+def CompOut():
+    print(Out = Dump1 % 1 + Dump2 % 1)
+
+inputcell(2, Input)
+CompOut()
