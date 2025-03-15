8976350fbd20ef508ef420f5f3cfc1f8d16e02bc
T_cat9000_2 <terpstragraham@gmail.com>
2025-03-15 12:38:19 -0700

    Update

--- divnumericcomputer.py
+++ divnumericcomputer.py
@@ -7,8 +7,11 @@ def inputcell(layer, input):
     Dump2 = input / layer
 
 def CompOut():
-    print(Out = Dump1 % 1 + Dump2 % 1)
+    Out = Dump1 % 1 + Dump2 % 1
+    print(Out)
     print(f"Dump {Dump1 - Dump1 % 1+Dump2 - Dump2 % 1}")
+    if Out == 1.0:
+        print("Even.")
 
 inputcell(2, Input)
 CompOut()
